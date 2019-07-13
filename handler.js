ba49c5f21075113d4d57230289fd6e00ad9dce1e
Jessan <jessan.cherian@gmail.com>
2019-07-13 01:08:33 -0700

    upload to s3 function

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const AWS = require('aws-sdk');
+
 module.exports.hello = async event => {
   return {
     statusCode: 200,
@@ -16,3 +18,27 @@ module.exports.hello = async event => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
+
+module.exports.uploadAudio = (event, context, callback) => {
+  var s3 = new AWS.S3();
+  var params = JSON.parse(event.body);
+
+  var s3Params = {
+    Bucket: 'summarizeitaudios',
+    Key:  params.name,
+    ContentType: params.type,
+    ACL: 'public-read',
+  };
+
+  var uploadURL = s3.getSignedUrl('putObject', s3Params);
+
+  callback(null, {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*' //probably only allow the AWS ec2 instance to access
+    },
+    body: JSON.stringify({ uploadURL: uploadURL }),
+  })
+};
+
+
